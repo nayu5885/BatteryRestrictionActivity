5bd9823937cc8238ba4b185eb8bf44bf6f5f212d
lifeistech <happymmw5885@gmail.com>
2021-06-19 15:19:18 +0900

    タイトル画面作成

---
+++ app/src/main/java/com/example/batteryrestrictionactivity/ManualActivity.kt
@@ -0,0 +1,13 @@
+package com.example.batteryrestrictionactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ManualActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manual)
+
+
+    }
+}
