daa5c601fb304425f3fb5dc846b14072306acbcb
lifeistech <happymmw5885@gmail.com>
2021-07-03 13:14:49 +0900

    button2を押してSettingActivityに遷移するコードを追加

--- app/src/main/java/com/example/batteryrestrictionactivity/MainActivity.kt
+++ app/src/main/java/com/example/batteryrestrictionactivity/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.batteryrestrictionactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button2: Button =findViewById(R.id.button2)
+
+            button2.setOnClickListener {
+                val intent = Intent(this, SettingActivity::class.java)
+                startActivity(intent)
+            }
+
     }
 }
